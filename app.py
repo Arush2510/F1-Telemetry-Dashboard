12a49cfd5ed288b9b6cb1ec92c1cf2a90203648c
Arush Dixit <121389635+Arush2510@users.noreply.github.com>
2025-07-02 10:05:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import plotly.express as px
 import plotly.graph_objects as go
-from scripts.telemetry_utils import get_session_data, get_telemetry_for_driver
+from telemetry_utils import get_session_data, get_telemetry_for_driver
 import fastf1
 import numpy as np
 import pandas as pd
